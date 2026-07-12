8dae768c3b637e247c962b76dfa7c4e3b2e90956
Busakrish <busakrish7237@gmail.com>
2026-07-12 13:22:20 +0530

    Delete client directory

--- client/src/components/allocation/index.js
+++
@@ -1,5 +0,0 @@
-// Allocation module components — export from here as they are created in Task 2
-// Example: export { default as AllocationTable } from './AllocationTable';
-// Example: export { default as AllocationForm  } from './AllocationForm';
-
-export {};
--- client/src/components/booking/index.js
+++
@@ -1,5 +0,0 @@
-// Booking module components — export from here as they are created in Task 2
-// Example: export { default as BookingCalendar } from './BookingCalendar';
-// Example: export { default as BookingForm    } from './BookingForm';
-
-export {};
